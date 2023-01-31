16934c415d197dd37999acfad455d7061446b02d
vitor1234567789 <120030239+vitor1234567789@users.noreply.github.com>
2023-01-31 11:34:48 -0300

    pular

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if(touches.length>0||keyDown("space") && trex.y >= height - 70) {
+    if(touches.length>0||keyDown("space") && trex.y >= height - 120) {
       trex.velocityY = -12;
       touches = []
     }
